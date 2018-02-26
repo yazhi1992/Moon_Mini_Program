a3356ac55961730aa15c1e6d047fd1226246d4fd
zengyazhi <yazhi1992@163.com>
2018-02-26 09:21:19 +0800

    增加年进度条功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,16 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var util = require('../../utils/util.js')
 
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    yearPercent:0,
+    yearTitle:""
   },
   //事件处理函数
   bindViewTap: function() {
@@ -42,6 +45,21 @@ Page({
         }
       })
     }
+    //计算今天是一年中的第几天
+    var date = new Date
+    var year = date.getFullYear()
+    var day = date.getDate()
+    //一般公历年份数是4的倍数就是闰年，否则是平年；
+    //但公历年份是整百数年数的必须是400的倍数才是闰年，不是400的倍数即为平年。
+    var isLeap = (0 !== year % 100) && (0 === year % 4) || (year % 400 == 0)
+    var fullYearDay = isLeap ? 366 : 365;
+    var today = Math.ceil((date - new Date(year.toString())) / (24 * 60 * 60 * 1000));
+    var test = parseInt(today / fullYearDay * 100)
+    this.setData({
+      yearPercent: test,
+      yearTitle: year +"年进度条"
+    })
+    console.log('day', day, year, fullYearDay, today, test)
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -49,6 +67,12 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  clikeHello: function() {
+    this.setData({
+      motto:'Hi',
+      yearPercent:50
     })
   }
 })
