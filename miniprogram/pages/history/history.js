e899f37f237dca2884b079b5d0f16d22245bc39d
zengyazhi <zengyazhiwork@163.com>
2018-09-12 17:54:31 +0800

    working on history

---
+++ miniprogram/pages/history/history.js
@@ -0,0 +1,71 @@
+// miniprogram/pages/history/history.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userImgUrl: "https://avatars1.githubusercontent.com/u/13739375?s=460&v=4",
+    userName: "名字",
+    imgUrl: "https://avatars1.githubusercontent.com/u/13739375?s=460&v=4",
+    content: "每天都想你",
+    comment: "DD回复辣辣：哈哈哈",
+    time: "2018-12-12"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
